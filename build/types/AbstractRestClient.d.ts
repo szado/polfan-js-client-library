71843c98008919491ec3899ac293c125344ddbba
Jarosław Żak <shado.ackerly@gmail.com>
2023-04-02 13:53:00 +0200

    next version of client

--- build/types/AbstractRestClient.d.ts
+++ build/types/AbstractRestClient.d.ts
@@ -11,7 +11,7 @@ export type HttpMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
 export declare abstract class AbstractRestClient {
     private readonly options;
     protected abstract defaultUrl: string;
-    protected constructor(options: RestClientOptions);
+    constructor(options: RestClientOptions);
     protected send<ResponseT = any>(method: HttpMethod, uri: string, data?: any): Promise<RestClientResponse<ResponseT>>;
     protected getUrl(uri: string): string;
     private removeStartingSlash;
